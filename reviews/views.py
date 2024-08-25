6f8fbdab0a481c8d2e302a4170e3f55321a1fdf1
Laney McGlohon <stitching.coder@gmail.com>
2024-08-25 16:31:07 -0700

    Add reviews and search

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, "base.html")
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html", {"search_text": search_text})
